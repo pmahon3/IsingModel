8e63face20bce648a10a474aaa8609365a58aada
pmahon3 <pmahon3@uwo.ca>
2020-03-09 16:27:32 -0400

    edits

--- Driver.m
+++ Driver.m
@@ -1,19 +1,25 @@
 %% Assignment 3
+% Driver using the MCMC and Lattice objects to simulate a 30x30 Ising model
+% at different temperatures and plots the results.
 close all;
+clear all;
 
+% Simulation Parameters
 nRows = 30;
 nCols = 30;
-nSims = 100;
+nSims = 4;
 nChains = 3;
 nThin = 10;
 nBurnin = 2000;
 nIterations = 3000;
 J = -1;
 
+% Array initialization for results
 Ts = linspace(0.1, 5, nSims);
 EResults = zeros(1, nSims);
 MResults = zeros(1, nSims);
 
+% Run the simulations
 for i = 1:nSims
     lat = Lattice(nRows,nCols, J, Ts(i), false);
     MC = MCMC( lat, 1, nIterations, nThin, nBurnin);
@@ -23,18 +29,43 @@ for i = 1:nSims
 end
 
 % Plot the results 
-plot(Ts, EResults, 'kx');
-xlabel("T");
-ylabel("<E>");
-title("System Energy");
-legend("Simulation");
 
+% Average energy per spin
+plot(Ts, EResults./-(nRows*nCols), 'kx');
+hold on; 
+
+% Compute the exact free energy per spin 
+% Source: en.wikipedia.org/wiki/Square-lattice_ising_model
+xDat = linspace(0.01, 5, 50);
+yDat = zeros(50);
+
+for i = 1:50
+    
+    xVal = 1/xDat(i);
+    k = 1 / (sinh(2*xVal))^2;
+    fInt = @(theta) 1 ./ sqrt( 1 - 4 .* k .* (( 1 + k ).^-2) * sin(theta).^2);
+    intVal = integral(fInt, 0, pi/2);
+    yDat(i) = -1 * coth(2*xVal)*(1 + (2/pi) * (2 * tanh(2 * xVal)^2 - 1) * intVal );
+end
+
+plot(xDat, yDat);
+
+xlabel("T");
+ylabel("<E>/N");
+title("System Energy per Spin");
+legend("Simulation", "Exact Solution");
+
+% Average magnitization per spin
 figure;
 plot(Ts, MResults./(nRows*nCols), 'kx');
-f = @(x) ( 1 - 1/(sinh(2/x))^2)^(1/8);
+
+% Compute the Osager-Yang solution
+f = @(x) ( 1 - 1./(sinh(2./x)).^2).^(1/8);
 hold on;
 fplot(f, [0 5]);
-T_c = 1/(log(1+sqrt(2)));
+
+% Compute the critical temperature
+T_c = 2/(log(1+sqrt(2)));
 xline(T_c);
 title("Magentization per Spin");
 xlabel("T");
--- MCMC.m
+++ MCMC.m
@@ -3,10 +3,20 @@
 classdef MCMC < handle
     
     % ATTRIBUTES
-    % lttc is a lattice object representing the current state of the chain.
-    % tmp is the tempurature of the lattice. 
-    % kb is the Boltzmann constant
-    
+    % currentLattice is the current state of the Markov chain
+    % previousLattice is the previous state of the Markov chain
+    % chainsE records the total Hamiltonian for each member of the chain
+    % chainsM records the total Magnetization for each member of the chain
+    % nChains is the number of chains
+    % nIterations is the number of non-burnin steps
+    % nThins is interval between succesive MC steps to accept a new state
+    % nBurnIn is the number of initial MC steps to reject
+    % size is the number of spins in the lattice ( dimensionality of the
+    % space)
+    % nSamples is the number of accepted MC steps
+    % chainsLat stores each lattice accepted to the chains
+    % E is the mean total Hamiltonian of all chains
+    % M is the mean absolute magnitization of all chains
     properties
      previousLattice;
         currentLattice; 
@@ -26,7 +36,10 @@ classdef MCMC < handle
     
     methods
         
+        % The constructor for the class
         function obj = MCMC( lattice, nChains, nIterations, nThin, nBurnin)
+            
+            % Initialize parameters
             obj.previousLattice = lattice;
             obj.currentLattice = lattice; 
             obj.nChains = nChains;
@@ -41,6 +54,9 @@ classdef MCMC < handle
             obj.E = 0;
         end
         
+        % runChains runs the MCMC simulation for each chain, taking the
+        % last lattice of each chain as the initial lattice of the new
+        % chain.
         function runChains( obj )
             
             % Burn in phase
@@ -67,15 +83,18 @@ classdef MCMC < handle
                     if ( mod(i, obj.nThin ) == 0 )
                         obj.chainsE(i/obj.nThin, j) = obj.currentLattice.E;
                         obj.chainsM(i/obj.nThin, j) = obj.currentLattice.M;
-                        image(obj.currentLattice.lattice, 'CDataMapping', 'scaled')
+                        %image(obj.currentLattice.lattice, 'CDataMapping', 'scaled');
                     end
                 end
             end
             
+            % Compute the mean total Hamiltonian and mean absolute
+            % magnetization for all chains
             obj.E = mean(mean(obj.chainsE));
             obj.M = mean(mean(abs(obj.chainsM)));
         end
         
+        % Plotting functions for verification
         function plotChainsE(obj)
             for j = 1:obj.nChains
                 plot(obj.chainsE(:, j));
