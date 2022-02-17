3fdca28ac0e4622b5545b39fbc3d1318aeab1826
Patrick Mahon <pmahon3@uwo.ca>
2022-02-17 10:08:39 -0500

    forgot to push contents

--- Driver.m
+++ Driver.m
@@ -1,50 +1,11 @@
 %% Assignment 3
-% Driver using the MCMC and Lattice objects to simulate a 30x30 Ising model
-% at different temperatures and plots the results.
-close all;
-clear all;
-
-% Simulation Parameters
-nRows = 30;
-nCols = 30;
-<<<<<<< HEAD
-nSims = 4;
-=======
-nSims = 50;
->>>>>>> 8e9e0c23450ebf211d609e61159b38641719324f
-nChains = 3;
-nThin = 10;
-nBurnin = 2000;
-nIterations = 3000;
-J = -1;
-
-% Array initialization for results
-Ts = linspace(0.1, 5, nSims);
-EResults = zeros(1, nSims);
-MResults = zeros(1, nSims);
-
-% Run the simulations
-for i = 1:nSims
-    lat = Lattice(nRows,nCols, J, Ts(i), false);
-    MC = MCMC( lat, 1, nIterations, nThin, nBurnin);
-    MC.runChains();
-    EResults(i) = MC.E;
-    MResults(i) = MC.M;
-end
-
-% Plot the results 
-
-% Average energy per spin
-plot(Ts, EResults./-(nRows*nCols), 'kx');
-<<<<<<< HEAD
-hold on; 
 
 % Compute the exact free energy per spin 
 % Source: en.wikipedia.org/wiki/Square-lattice_ising_model
-xDat = linspace(0.01, 5, 50);
-yDat = zeros(50);
+xDat = linspace(0.01, 5, 100);
+yDat = zeros(100);
 
-for i = 1:50
+for i = 1:100
     
     xVal = 1/xDat(i);
     k = 1 / (sinh(2*xVal))^2;
@@ -53,32 +14,12 @@ for i = 1:50
     yDat(i) = -1 * coth(2*xVal)*(1 + (2/pi) * (2 * tanh(2 * xVal)^2 - 1) * intVal );
 end
 
-plot(xDat, yDat);
-
-xlabel("T");
-ylabel("<E>/N");
-title("System Energy per Spin");
-legend("Simulation", "Exact Solution");
-=======
-xlabel("T");
-ylabel("<E>/N");
-title("System Energy per Spin");
-legend("Simulation");
->>>>>>> 8e9e0c23450ebf211d609e61159b38641719324f
-
-% Average magnitization per spin
-figure;
-plot(Ts, MResults./(nRows*nCols), 'kx');
-
+csvwrite("Energy.csv", yDat);
+T_c = 2/(log(1+sqrt(2)));
 % Compute the Osager-Yang solution
-f = @(x) ( 1 - 1./(sinh(2./x)).^2).^(1/8);
-hold on;
-fplot(f, [0 5]);
+xDat2 = linspace(0.01, T_c, 100);
+yDat2 =  (1 - 1./(sinh(2./xDat2)).^4).^(1/8);
+csvwrite("Mag.csv", [xDat2', yDat2']);
 
 % Compute the critical temperature
-T_c = 2/(log(1+sqrt(2)));
-xline(T_c);
-title("Magentization per Spin");
-xlabel("T");
-ylabel("<|M|>");
-legend("Simulation", "Osager-Yang Solution", "T_c");
+
